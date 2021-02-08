883144b6e308d29ba6b89e901aca3a8b2d552f56
derek <lideyu888@163.com>
2021-02-08 23:43:18 +0800

    deploy

---
+++ precache-manifest.88b5d56d78226fd794941384c13eea61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fb5d091d8707c0e700",
+    "url": "/iChord/css/app.66b4a5fd.css"
+  },
+  {
+    "revision": "aa51bda33fa3ab53eb09",
+    "url": "/iChord/css/chunk-vendors.b29c2702.css"
+  },
+  {
+    "revision": "be16178ab7ea18972685d11d97b993b6",
+    "url": "/iChord/index.html"
+  },
+  {
+    "revision": "e4fb5d091d8707c0e700",
+    "url": "/iChord/js/app.e360c1fb.js"
+  },
+  {
+    "revision": "aa51bda33fa3ab53eb09",
+    "url": "/iChord/js/chunk-vendors.74a3b730.js"
+  },
+  {
+    "revision": "eb971c2e4f5760e911ecaa3e586dbf7a",
+    "url": "/iChord/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/iChord/robots.txt"
+  }
+]);
